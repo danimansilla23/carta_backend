72822ab7060bddb9d28724d01fa6545d9fbfe6ec
Dani Mansilla <danimansilla52@gmail.com>
2025-07-18 17:15:17 -0300

    export-data

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+export default () => ({
+  'strapi-csv-import-export': {
+    enabled: true,
+    config: {
+      authorizedExports: [
+        'api::categoria.categoria',
+        'api::plato-del-dia.plato-del-dia',
+        'api::producto.producto',
+        'api::salsas-para-pasta.salsas-para-pasta',
+        'api::salsas-para-plato.salsas-para-plato',
+      ],
+      authorizedImports: [
+        'api::categoria.categoria',
+        'api::plato-del-dia.plato-del-dia',
+        'api::producto.producto',
+        'api::salsas-para-pasta.salsas-para-pasta',
+        'api::salsas-para-plato.salsas-para-plato',
+      ],
+    },
+  },
+});
